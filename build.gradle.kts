52ab4109c28ebf85e07217836a98c045c6e2bc86
ahmadasrofulfadli <154332310+ahmadasrofulfadli@users.noreply.github.com>
2025-06-14 11:37:27 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-}
-plugins {
 alias(libs.plugins.google.gms.google.services) apply false
 alias(libs.plugins.google.firebase.crashlytics) apply false
 }
